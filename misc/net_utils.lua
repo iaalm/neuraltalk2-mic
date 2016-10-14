7149e4050c43b10b1c40ab591a3c6b4bb6c44c20
iaalm <iaalmsimon@gmail.com>
2016-10-14 12:35:27 +0800

    rm .json and _out.json in coco-caption

--- misc/net_utils.lua
+++ misc/net_utils.lua
@@ -173,6 +173,7 @@ function net_utils.language_eval(predictions, id)
   os.execute('./misc/call_python_caption_eval.sh val' .. id .. '.json') -- i'm dying over here
   local result_struct = utils.read_json('coco-caption/val' .. id .. '.json_out.json') -- god forgive me
   os.remove('coco-caption/val' .. id .. '.json_out.json')
+  os.remove('coco-caption/val' .. id .. '.json')
   return result_struct
 end
 
