63fd20fc2421e92b1df2b4cb66670adde06cddde
iaalm <iaalmsimon@gmail.com>
2016-10-17 10:38:18 +0800

    back lstm res and impove time dropout

--- misc/LSTM.lua
+++ misc/LSTM.lua
@@ -20,12 +20,17 @@ function LSTM.lstm(input_size, output_size, rnn_size, n, dropout_l, dropout_t, r
     -- c,h from previos timesteps
     local prev_h = inputs[L*2+1]
     local prev_c = inputs[L*2]
+    if dropout_t > 0 then prev_h = nn.Dropout(dropout_t)(prev_h):annotate{name='drop_t_' .. L} end -- apply dropout_t, if any
     -- the input to this layer
     if L == 1 then 
       x = inputs[1]
       input_size_L = input_size
     else 
+      if res_rnn > 0 and L > res_rnn + 1 and (L - 2) % res_rnn == 0 then    
+        x = nn.CAddTable()({outputs[(L-1)*2], outputs[(L-1-res_rnn)*2]})    
+      else
         x = outputs[(L-1)*2] 
+      end
       if dropout_l > 0 then x = nn.Dropout(dropout_l)(x):annotate{name='drop_l_' .. L} end -- apply dropout_l, if any
       input_size_L = rnn_size
     end
@@ -58,12 +63,7 @@ function LSTM.lstm(input_size, output_size, rnn_size, n, dropout_l, dropout_t, r
     local next_h = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
     
     table.insert(outputs, next_c)
-    if dropout_t > 0 then next_h = nn.Dropout(dropout_t)(next_h):annotate{name='drop_t_' .. L} end -- apply dropout_t, if any
-    if res_rnn > 0 and L > res_rnn and (L - 1) % res_rnn == 0 then
-      table.insert(outputs, nn.CAddTable()({next_h, outputs[(L-res_rnn)*2]}))
-    else
-      table.insert(outputs, next_h)
-    end
+    table.insert(outputs, next_h)
   end
 
   -- set up the decoder
