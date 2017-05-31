4502928ef5168749081754ed9934ff1fa8bdfea4
iaalm <iaalmsimon@gmail.com>
2017-05-31 09:04:33 +0800

    new log_show.py

--- log_show.py
+++ log_show.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+#!env python3
 
 import json
 import sys
 import os
 import re
 import argparse
+from itertools import cycle
+from termcolor import cprint
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", action='store_false', help="Not sort by CIDEr")
@@ -12,6 +14,10 @@ parser.add_argument("-n", action='store_false', help="Not skip nan")
 parser.add_argument("dirs", metavar='dir' ,nargs='+', help="model dirs")
 args = parser.parse_args()
 
+if args.s:
+    selected_metric = "CIDEr"
+else:
+    selected_metric = "Bleu_4"
 val_name = ["CIDEr","Bleu_1","Bleu_2","Bleu_3","Bleu_4","METEOR","ROUGE_L"]
 table = []
 for start_filename in args.dirs:
@@ -20,6 +26,8 @@ for start_filename in args.dirs:
         filename = os.path.join(start_filename, 'model_.json')
         if not os.path.exists(filename):
             filename = os.path.join(start_filename, 'model_id.json')
+        if not os.path.exists(filename):
+            continue
         dirs = [filename]
         rrr = re.compile(r'\.t7$')
         if not(len(sys.argv) > 2 and sys.argv[2] == '-n'):
@@ -45,11 +53,14 @@ for start_filename in args.dirs:
             for name in val_name:
                 max_result = 0
                 for k,v in val_data:
-                    if name == 'CIDEr' and float(v[name]) > max_result:
+                    if name == selected_metric and float(v[name]) > max_result:
                         max_cider = startx + int(k)
                     max_result = max(max_result, float(v[name]))
                 val_max[name] = max_result
-            startx = startx + int(val_data[-1][0])
+            try:
+                startx = startx + int(val_data[-1][0])
+            except IndexError:
+                pass
     except FileNotFoundError:
         if args.n:
             continue
@@ -59,15 +70,15 @@ for start_filename in args.dirs:
     table.append((start_filename, val_max, finetune_start, max_cider))
 
 # output
-max_name_len = max([len(i) for i in args.dirs])
+max_name_len = max([len(os.path.basename(i)) for i in args.dirs]) + 1
 print(" "*(max_name_len), end="")
 for name in val_name:
     print("\t"+name, end="")
 print("\t  pos(k)")
-if args.s:
-    table = sorted(table, key=lambda x:x[1]['CIDEr'], reverse=True)
-for i in table:
-    print(("%"+str(max_name_len)+"s")%os.path.basename(i[0]), end="")
+table = sorted(table, key=lambda x: x[1][selected_metric], reverse=True)
+for i, c in zip(table, cycle(['red', 'yellow', 'green',
+                              'cyan', 'blue', 'magenta'])):
+    cprint(("%"+str(max_name_len)+"s")%os.path.basename(i[0]), c, end="")
     for name in val_name:
-        print("\t%.3f"%i[1][name], end="")
-    print("\t%4.0f+%3.0f"%((i[3]-i[2])/1000, i[2]/1000))
+        cprint("\t%.1f"%(i[1][name]*100), c, end="")
+    cprint("\t%4.0f+%3.0f"%((i[3]-i[2])/1000, i[2]/1000), c)
